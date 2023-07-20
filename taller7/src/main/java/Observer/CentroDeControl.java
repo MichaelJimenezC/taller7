84bd5f8e20b575a03652cc3e4843610db64eabcb
micjejim <micjejim@espol.edu.ec>
2023-07-19 22:14:00 -0500

    observer

---
+++ taller7/src/main/java/Observer/CentroDeControl.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Observer;
+
+import FactoryMethod.Tarea;
+import java.util.List;
+
+/**
+ *
+ * @author Michael
+ */
+public class CentroDeControl {
+    private List<Notificador> lista;
+    
+    public void suscribir(Notificador n){
+        
+    }
+    public void desuscribir(Notificador n){
+        
+    }
+    public void notificarCambio(Tarea t){
+        
+    }
+}
---
+++ taller7/src/main/java/Observer/NotificacionSMS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Observer;
+
+/**
+ *
+ * @author Michael
+ */
+public class NotificacionSMS extends Notificacion{
+    private String numeroCelular;
+    
+    @Override
+    public void enviarNotificacion() {
+        
+    }
+}
